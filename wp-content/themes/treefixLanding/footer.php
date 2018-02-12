777560704ce4b96f56a856c192ccf7821218bfae
treefix <rodrigo.pardo@treefix.com>
2018-02-12 02:52:34 -0800

    First Migration

---
+++ wp-content/themes/treefixLanding/footer.php
@@ -0,0 +1,8 @@
+
+
+	</div><!-- #content -->
+</div><!-- end #page or .site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/treefixLanding/front-page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<div class="landing-page">
+	<div class="centeredGrid">
+		<h1 class="logoContainer">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/treeFixLogoWhite@2x.png" alt="TreeFix">
+		</h1>
+		<p class="comingSoonText">Coming Soon</p>
+	</div>
+
+</h1>
+
+
+<?php get_footer();?>
